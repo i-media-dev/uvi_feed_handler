fc80e8246ac5a6c69fcc5e367a197e0097cbdb46
i-media-dev <mrk_dev@i-media.ru>
2025-10-03 13:14:53 +0300

    PRO-87: optimized logic of replace image method (deleted one iteration) and separate method get dict

--- handler/feeds_handler.py
+++ handler/feeds_handler.py
@@ -29,16 +29,6 @@ class XMLHandler(FileMixin):
         self.new_feeds_folder = new_feeds_folder
         self.new_image_folder = new_image_folder
 
-    # def _save_xml(self, elem, file_folder, filename) -> None:
-    #     """Защищенный метод, сохраняет отформатированные файлы."""
-    #     file_path = self._make_dir(file_folder)
-    #     tree = ET.ElementTree(elem)
-    #     tree.write(
-    #         file_path / f'new_{filename}',
-    #         encoding='utf-8',
-    #         xml_declaration=True
-    #     )
-
     def _save_xml(self, elem, file_folder, filename) -> None:
         """Защищенный метод, сохраняет отформатированные файлы."""
         root = elem
@@ -87,29 +77,44 @@ class XMLHandler(FileMixin):
             logging.error(f'Ошибка в _delete_picture: {e}')
             raise
 
+    def _get_image_dict(self):
+        image_dict = {}
+        for img_file in self._get_filenames_list(
+            self.new_image_folder,
+            'png'
+        ):
+            try:
+                offer_id = img_file.split('_')[0]
+                if offer_id not in image_dict:
+                    image_dict[offer_id] = []
+                image_dict[offer_id].append(img_file)
+            except (ValueError, IndexError):
+                logging.warning(
+                    'Не удалось присвоить изображение '
+                    f'{img_file} ключу {offer_id}'
+                )
+                continue
+            except Exception as e:
+                logging.error(
+                    'Неожиданная ошибка во время '
+                    f'сборки словаря image_dict: {e}'
+                )
+                raise
+        return image_dict
+
     @time_of_function
     def image_replacement(self):
         """Метод, подставляющий в фиды новые изображения."""
         try:
-            image_dict = {}
-            for img_file in self._get_filenames_list(
-                self.new_image_folder,
-                'png'
-            ):
-                try:
-                    offer_id = img_file.split('_')[0]
-                    if offer_id not in image_dict:
-                        image_dict[offer_id] = []
-                    image_dict[offer_id].append(img_file)
-                except (ValueError, IndexError):
-                    continue
+            image_dict = self._get_image_dict()
 
             for file_name in self._get_filenames_list(self.feeds_folder):
                 tree = self._get_tree(file_name, self.feeds_folder)
                 root = tree.getroot()
 
-                for offer in list(root.findall('.//offer')):
-                    offer_id = offer.get('id')
+                offers = list(root.findall('.//offer'))
+                for offer in offers:
+                    offer_id = offer.get('id').strip()
                     if not offer_id:
                         continue
 
@@ -121,16 +126,11 @@ class XMLHandler(FileMixin):
                         f'удалено {len(images)} изображений'
                     )
 
-                for offer in list(root.findall('.//offer')):
-                    offer_id = offer.get('id')
-                    if not offer_id:
-                        continue
-
                     if offer_id in image_dict:
                         for img_file in image_dict[offer_id]:
                             picture_tag = ET.SubElement(offer, 'picture')
                             picture_tag.text = (
-                                f'domen_name.i-media/директория/{img_file}'
+                                f'https://domen_name.i-media/directory/{img_file}'
                             )
                             logging.info(
                                 f'В оффер с id {offer_id} добавлена {img_file}'
