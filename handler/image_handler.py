30a337e53eca76a930866f1541fbd2bb4e709167
i-media-dev <mrk_dev@i-media.ru>
2025-10-14 11:31:47 +0300

    fix

--- handler/image_handler.py
+++ handler/image_handler.py
@@ -119,7 +119,6 @@ class XMLImage(FileMixin):
         try:
             self._build_offers_set(
                 self.image_folder,
-                'jpeg',
                 self._existing_image_offers
             )
         except (DirectoryCreationError, EmptyFeedsListError):
@@ -192,7 +191,6 @@ class XMLImage(FileMixin):
         try:
             self._build_offers_set(
                 self.new_image_folder,
-                'png',
                 self._existing_framed_offers
             )
         except (DirectoryCreationError, EmptyFeedsListError):
